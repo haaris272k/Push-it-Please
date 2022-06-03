f76e2d3524d943810f7484c7f7ae76a89c336f8b
haaris272k <javedhaaris20@gmail.com>
2022-06-03 15:09:52 +0530

    Modified

--- Winscript.bat
+++ Winscript.bat
@@ -3,11 +3,12 @@
 @REM # Simple Script to automate the push of code to github #
 @REM ########################################################
 
+echo off
 git status
 git add -A
 git commit -m "Modified"
 git push
 echo "Done, status is: "
 git status
-
+echo off
 echo "That saved hell a lot of typing labour >_<"
